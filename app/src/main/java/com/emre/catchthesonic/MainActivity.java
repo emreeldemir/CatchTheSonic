5aa894c6f24cff88373b4741ebe6bc5561e71a6b
Emre Eldemir <emreeldemir893@gmail.com>
2023-02-03 00:24:58 +0300

    imageView array created

--- app/src/main/java/com/emre/catchthesonic/MainActivity.java
+++ app/src/main/java/com/emre/catchthesonic/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView timeText, scoreText;
+    ImageView imageView, imageView2, imageView3, imageView4, imageView5, imageView6,
+              imageView7, imageView8, imageView9;
+
+    ImageView[] imageArray;
     int score;
 
     @Override
@@ -19,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         timeText = findViewById(R.id.timeText);
         scoreText = findViewById(R.id.scoreText);
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView, imageView2, imageView3, imageView4, imageView5,
+                                      imageView6, imageView7, imageView8, imageView9};
+
         score = 0;
 
         new CountDownTimer(10000, 1000) {
