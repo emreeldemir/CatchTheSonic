cf67de703f5cc7b64442ad1fc2747e06a602ad4a
Emre Eldemir <emreeldemir893@gmail.com>
2023-02-03 00:08:03 +0300

    grid layout added and edited

--- app/src/main/java/com/emre/catchthesonic/MainActivity.java
+++ app/src/main/java/com/emre/catchthesonic/MainActivity.java
@@ -3,12 +3,30 @@ package com.emre.catchthesonic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeText, scoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+
     }
+
+    public void increaseScore(View view) {
+
+
+
+    }
+
+
+
+
 }
